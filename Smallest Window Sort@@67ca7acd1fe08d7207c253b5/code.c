9e16b25d152811e2792f1dbfb112067a4aed75c3
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-03-28 19:02:06 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,28 +1,39 @@
 // Your code here...
 
 int findUnsortedSubarray(int arr[], int n){
-    int arr2[n];
-    int j = 0 ;
-    for(int i = n-1; i < n ; i ++){
-        arr2[j] = arr[i];
-        j++;
-    }
-    int l = 0 ; int r = n-1;
-    int first = 0;
-    int second = 0;
-    for(int i = 0 ; i < n-1; i ++ ){
+    // int arr2[n];
+    // int j = 0 ;
+    // for(int i = n-1; i < n ; i ++){
+    //     arr2[j] = arr[i];
+    //     j++;
+    // }
+    // int l = 0 ; int r = n-1;
+    // int first = 0;
+    // int second = 0;
+    // for(int i = 0 ; i < n-1; i ++ ){
+    //     if(arr[i+1] < arr[i]){
+    //         first = i;
+    //         break;
+    // }
+    // }
+    // for(int i = 0 ; i < n-1; i ++ ){
+    //     if(arr[i+1] > arr[i]){
+    //         second = n- i-1;
+    //         break;
+    // }
+    // }
+    // return second - first;
+    int sorted= n+1;
+    for(int i  =0 ; i < n-1 ; i ++){
         if(arr[i+1] < arr[i]){
-            first = i;
-            break;
+            sorted = i;
+        }
     }
+    if(sorted != n+1){
+        return 0;
     }
-    for(int i = 0 ; i < n-1; i ++ ){
-        if(arr[i+1] > arr[i]){
-            second = n- i-1;
-            break;
-    }
-    }
-    return second - first;
+
+    return 5;
 
     
 }
