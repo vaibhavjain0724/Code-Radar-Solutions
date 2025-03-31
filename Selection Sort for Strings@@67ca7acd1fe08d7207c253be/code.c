725b95633400e5d637c9d8d0081e56d5b533a65d
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-03-31 13:46:22 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,10 +1,27 @@
 // Your code here...
-#include <string.h>
+// #include <string.h>
+
+int compare_string(char* str1, char *str2){
+    int i = 0;
+
+    while(str1[i] && str2[i] ){
+        if(str1[i] > str2[i]){
+            return 2;
+        }
+        else if(str1[i] > str2[i]){
+            i++;
+        }
+        else{
+            return 1;
+        }
+        return 0;
+    }
+}
 void selectionSort(char *arr, int n){
     int min_index = 0;
     for(int i = 0; i < n ; i++){
         for(int j = 0; j < n ; j++){
-            if(strcmp(arr[min_index], arr[j])){
+            if(compare_string(arr[min_index], arr[i]) == 2){
                 min_index = j;
             }
         }
