b07fc58c544cbc509159b6932411574bef45ab3c
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-03-31 22:35:59 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -1,20 +1,49 @@
-// Your code here...
-#include <stdio.h>
-int main(){
+// // Your code here...
+// #include <stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i = 0; i < n; i++){
+//         scanf("%d", &arr[i]);
+//     }
+//     int target;
+//     int index= -1;
+//     scanf("%d", &target);
+//     for(int i = 0 ; i < n ; i++){
+//         if(arr[i] == target){
+//             index= i;
+//             break;
+//         }
+//     }
+//     printf("%d", index);
+// }
+
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
     int n;
     scanf("%d",&n);
     int arr[n];
-    for(int i = 0; i < n; i++){
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
     int target;
-    int index= -1;
-    scanf("%d", &target);
-    for(int i = 0 ; i < n ; i++){
-        if(arr[i] == target){
-            index= i;
+    scanf("%d",&target);
+    int idx = -1
+    bool flag = false;
+    for(int i=0;i<n;i++){
+        if(arr[i]==target){
+            flag = true;
+            idx = i;
             break;
         }
+        if(flag==false){
+            printf("%d",x);
+        }
+        else{
+            printf("%d",idx)
+        }
     }
-    printf("%d", index);
 }
