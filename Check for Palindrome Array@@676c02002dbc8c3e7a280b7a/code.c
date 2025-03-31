0578059c06d8218d3463eea99946d45c0926bbfd
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-04-01 03:05:16 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,22 +1,52 @@
-// Your code here..
-#include <stdio.h>
-int main(){
-    int n ;
-    scanf("%d", &n);
-    int arr[n];
-    for(int i = 0 ; i < n ; i++){
-        scanf("%d", &arr[i]);
-    }
-    int l = 0; int r = n-1;
-    char *ans = "YES";
-    while(l<= r){
-        if(arr[l] != arr[r]){
-            ans = "NO";
-            break;
-        }
-        l++;
-        r--;
-    }
-    printf("%s", ans);
+// // Your code here..
+// #include <stdio.h>
+// int main(){
+//     int n ;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for(int i = 0 ; i < n ; i++){
+//         scanf("%d", &arr[i]);
+//     }
+//     int l = 0; int r = n-1;
+//     char *ans = "YES";
+//     while(l<= r){
+//         if(arr[l] != arr[r]){
+//             ans = "NO";
+//             break;
+//         }
+//         l++;
+//         r--;
+//     }
+//     printf("%s", ans);
 
+// }
+
+
+#include<stdio.h>
+void palindrome(int arr[],int n){
+    int i=0;
+    int j=n-1;
+    while(i<=j){
+        if(arr[i]!=arr[j]){
+            return 0;
+        }
+        i++;
+        j--;
+    }
+    return -1;
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    if(palindrome(arr,n)){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
 }
