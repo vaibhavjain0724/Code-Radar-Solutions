35d83138efc9132f36b165afd5830da741a3cf46
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-03-31 19:19:25 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -4,10 +4,11 @@
 #include <ctype.h>
 char* caesarCipher(char message[100],int shift,char encrypted[100]){
     int i = 0;
-    int base = 97;
+    char base = 'a';
     
     while(message[i] != '\0'){
-        if(isalpha(message[i])){            
+        if(isalpha(message[i])){ 
+            base = message[i] ? 'a' : 'A';           
             encrypted[i] = ((message[i] - base + shift) % 26) + base; 
         }
         i++;
