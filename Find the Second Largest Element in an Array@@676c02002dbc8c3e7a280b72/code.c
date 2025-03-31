198de909e4e2528e80438424925ea04c92a51081
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-04-01 00:49:33 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,30 +1,60 @@
-// Your code here...
-#include <stdio.h>
-#include <limits.h>
-int main(){
-    int n;
-    // int ans = -1;
-    scanf("%d", &n);
-    int arr[n];
-    for(int i= 0; i < n ; i++){
-        scanf("%d", &arr[i]);
-    }
-    int max =   INT_MIN;
-    int smax = INT_MIN;
-    for(int i = 0 ; i < n ; i++){
-        if(arr[i] > max){
-            smax = max;
-            max= arr[i];
-        }
-        else if(arr[i] != max && arr[i] > smax){
-            smax= arr[i];
-        }
+// // Your code here...
+// #include <stdio.h>
+// #include <limits.h>
+// int main(){
+//     int n;
+//     // int ans = -1;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for(int i= 0; i < n ; i++){
+//         scanf("%d", &arr[i]);
+//     }
+//     int max =   INT_MIN;
+//     int smax = INT_MIN;
+//     for(int i = 0 ; i < n ; i++){
+//         if(arr[i] > max){
+//             smax = max;
+//             max= arr[i];
+//         }
+//         else if(arr[i] != max && arr[i] > smax){
+//             smax= arr[i];
+//         }
         
+//     }
+//     if(n == 1 || smax == INT_MIN){
+//         printf("%d", -1);
+//     }
+//     else{
+//     printf("%d",smax);
+//     }
+// }
+
+
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-    if(n == 1 || smax == INT_MIN){
-        printf("%d", -1);
+    int max = INT_MIN;
+    int smax = INT_MIN;
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            smax = max;
+            max = arr[i];
+        }
+        else if(arr[i]<smax && arr[i]!=max){
+            smax = arr[i];
+        }
+    }
+    if(n==1 || smax == INT_MIN){
+        printf("%d",-1);
     }
     else{
-    printf("%d",smax);
+        printf("%d",smax);
     }
 }
