34042d1037dc39677a3034e72a01aedb2c728719
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-04-10 20:45:52 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -15,7 +15,7 @@ int main(){
         l++;
     }
     int max = 0;
-    for(int i = 0 ; i < 27; i++){
+    for(int i = 1 ; i < 27; i++){
         if(arr[i] > max){
             max = arr[i];
         }
